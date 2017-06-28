1eff6a764aef8dbd58113774ac4970811bab7af5
alaaDragneel <moaalaa16@gmail.com>
2017-06-28 09:23:16 +0200

    download node

---
+++ nodejs.sh
@@ -0,0 +1,4 @@
+#! extract the tar.xz file first then name the folder to node then run the next command
+
+sudo cp -r node/{bin,include,lib,share} /usr/
+
--- xampp.sh
+++ xampp.sh
@@ -2,4 +2,9 @@
 ~/Downloads$ sudo su
 	sudo chmod 777 -R {xampp file name by extenxion}
 ./{xampp file name by extenxion}
-	
+
+
+composer require barryvdh/laravel-debugbar
+
+Barryvdh\Debugbar\ServiceProvider::class,
+
